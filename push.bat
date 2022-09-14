aabcaf96fe8779ebe5db957ca0b826d103c7954b
CIT142F-18@M307.UC-BCF.EDU.PH <CIT142F-18@M307.UC-BCF.EDU.PH>
2022-09-14 17:42:49 +0800

    -batch upload

--- push.bat
+++ push.bat
@@ -1,5 +1,5 @@
 @echo off
-git remote set-url origin https://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@github.com/valenciakirst/practice-rep.git
+git remote set-url origin https://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@github.com/valenciakirst/practice-rep.git
 pause
 git add . 
 git commit -m -"batch upload"
